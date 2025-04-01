9443acb83f2a7d680614ae3fb9818e0ceab00fd1
ZhijianZhou <1275747829@qq.com>
2025-04-01 16:59:36 +0800

    xtb

---
+++ xtb-python/xtb/__init__.py
@@ -0,0 +1,22 @@
+# This file is part of xtb.
+#
+# Copyright (C) 2020 Sebastian Ehlert
+#
+# xtb is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# xtb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with xtb.  If not, see <https://www.gnu.org/licenses/>.
+"""Python API of the extended tight binding program package"""
+
+from importlib import metadata
+
+__version__ = metadata.version(__name__)
+API_VERSION = "1.0.0"
