8f023bd196cb9fa40b5553fdea87f7b0e6bd5726
ZhijianZhou <1275747829@qq.com>
2025-05-29 17:16:30 +0800

    example

---
+++ example/edm_ddpo_xtb/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python main_edm.py --wandb \
+       --wandb_name edm_ddpo_xtb_test
+
---
+++ example/edm_ddpo_xtb/run_eval.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source activate
+conda activate py310
+
+MODEL_PATHS=(
+    "./exp/edm-dft"
+)
+
+STEP_SETS=(
+    "1000"
+)
+
+MODEL_NAME=(
+    "generative_model_ema.npy"
+)
+
+N_SAMPLES=10000
+
+for model_path in "${MODEL_PATHS[@]}"; do
+    for steps in "${STEP_SETS[@]}"; do
+        for model_name in "${MODEL_NAME[@]}"; do
+            echo "Running eval_analyze.py with model: $model_path, steps: $steps, MODEL_NAME $model_name"
+            python -m Model.EDM.eval_analyze --model_path "$model_path" --n_samples "$N_SAMPLES" --steps "$steps" --save_to_xyz True --model_name "${model_name}"
+        done
+    done
+done
+
+echo "All evaluations completed!"
