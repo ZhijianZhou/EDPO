ff7e1777e2e52556016ce882daca6512f643cfa2
ZhijianZhou <1275747829@qq.com>
2025-05-29 18:22:13 +0800

    example

--- example/edm_ddpo_xtb/run_eval.sh
+++ example/edm_ddpo_xtb/run_eval.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-source activate
-conda activate py310
+cd ./Model/EDM
 
 MODEL_PATHS=(
     "./exp/edm-dft"
@@ -15,13 +13,13 @@ MODEL_NAME=(
     "generative_model_ema.npy"
 )
 
-N_SAMPLES=10000
+N_SAMPLES=100
 
 for model_path in "${MODEL_PATHS[@]}"; do
     for steps in "${STEP_SETS[@]}"; do
         for model_name in "${MODEL_NAME[@]}"; do
             echo "Running eval_analyze.py with model: $model_path, steps: $steps, MODEL_NAME $model_name"
-            python -m Model.EDM.eval_analyze --model_path "$model_path" --n_samples "$N_SAMPLES" --steps "$steps" --save_to_xyz True --model_name "${model_name}"
+            python eval_analyze.py --model_path "$model_path" --n_samples "$N_SAMPLES" --steps "$steps" --save_to_xyz True --model_name "${model_name}"
         done
     done
 done
